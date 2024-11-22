23700e5a0ccace4399d7c6374de8581c3dc81734
nonz250 <hosaka.non.work@gmail.com>
2024-11-23 02:51:09 +0900

    fix: html をちょっとスリムに

--- src/sub-window/index.php
+++ src/sub-window/index.php
@@ -25,43 +25,34 @@ $csrfToken = generateCsrfToken();
 <h1>お客様フォーム例</h1>
 
 <form action="/sub-window/create-customer.php" method="post">
-    <div style="display: grid; gap: 1rem; align-content: center;">
-        <div>
-            <label for="customer-name">お客様名 <input id="customer-name" type="text" name="customer_name"/></label>
-        </div>
+    <label for="customer-name">お客様名 <input id="customer-name" type="text" name="customer_name"/></label>
 
-        <div style="display: flex; gap: 1rem; align-items: center;">
-            <div>
-                <script
-                    type="text/javascript"
-                    src="https://checkout.pay.jp/prerelease"
-                    data-payjp-key="<?php echo htmlspecialchars($_ENV['PAYJP_PUBLIC_KEY'] ?? ''); // `pk_` から始まる公開鍵を設定してください。 ?>"
-                    data-payjp-three-d-secure="true"
-                    data-payjp-three-d-secure-workflow="subwindow"
-                    data-payjp-extra-attribute-email
-                    data-payjp-extra-attribute-phone
-                    data-payjp-partial="true"
-                    data-payjp-on-created="onCreatedToken"
-                ></script>
-            </div>
-            <div>
-                <small>※ <?php echo htmlspecialchars('<input type="hidden" name="payjp-token">'); ?> の value に token が自動的にセットされます。</small><br />
-                <small>※ 生成されたトークン: <span id="created-token"></span></small>
-            </div>
+    <div style="display: flex; gap: 1rem; align-items: center;">
+        <div>
+            <script
+                type="text/javascript"
+                src="https://checkout.pay.jp/prerelease"
+                data-payjp-key="<?php echo htmlspecialchars($_ENV['PAYJP_PUBLIC_KEY'] ?? ''); // `pk_` から始まる公開鍵を設定してください。 ?>"
+                data-payjp-three-d-secure="true"
+                data-payjp-three-d-secure-workflow="subwindow"
+                data-payjp-extra-attribute-email
+                data-payjp-extra-attribute-phone
+                data-payjp-partial="true"
+                data-payjp-on-created="onCreatedToken"
+            ></script>
+        </div>
+        <div>
+            <small>※ <?php echo htmlspecialchars('<input type="hidden" name="payjp-token">'); ?> の value に token が自動的にセットされます。</small><br />
+            <small>※ 生成されたトークン: <span id="created-token"></span></small>
         </div>
     </div>
-
     <br />
-
     <input id="customer-email" type="hidden" name="customer_email">
     <input type="hidden" name="csrf_token" value="<?php echo htmlspecialchars($csrfToken); ?>" />
     <button type="submit">登録</button>
 </form>
-
 <br />
-
 <a href="/">戻る</a>
-
 </body>
 </html>
 
