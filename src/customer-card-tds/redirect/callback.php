d3e9ad0dbb19326f9a23ffefdd7b2817d6a04282
nonz250 <nozomi.hosaka@binc.jp>
2025-01-07 10:11:02 +0900

    docs: コメントを修正

--- src/customer-card-tds/redirect/callback.php
+++ src/customer-card-tds/redirect/callback.php
@@ -11,7 +11,7 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
 
 session_start();
 
-// セッションから保持していた顧客IDを再取得します。
+// セッションから保持していた3DセキュアリクエストIDを再取得します。
 $threeDSecureRequestId = $_SESSION['tds_input_data']['three_d_request_id'] ?? '';
 unset($_SESSION['tds_input_data']);
 
